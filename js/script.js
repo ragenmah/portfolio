c9d06b4524bbb7b094357e453b9d248691c1fb2a
ragenmah <ragenmah99@gmail.com>
2022-01-25 14:52:30 +0545

    theme adding is in progress

--- js/script.js
+++ js/script.js
@@ -7,5 +7,18 @@
 
 /*******************custom css **********/
 
+var themeName = window.localStorage.getItem("themeName");
 
+if (themeName == "style") {
+  // window.localStorage.setItem("themeName", "style");
+  document.head.innerHTML = '<link rel="stylesheet" href="../css/style.css"/>';
+} 
+else {
+  document.head.innerHTML = '<link rel="stylesheet" href="../css/light.css"/>';
+}
 
+function toggleTheme() {
+  // window.localStorage.clear();
+  window.localStorage.setItem("themeName", "dark");
+  document.head.innerHTML = '<link rel="stylesheet" href="../css/light.css"/>';
+}
